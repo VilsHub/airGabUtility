281c75b496567d009e1d07cd928e82febc0ff4e0
starlyvil <starlyvil@gmail.com>
2026-03-05 17:26:09 +0100

    optimised prunning logic

--- local_src/groupPruneImages.sh
+++ local_src/groupPruneImages.sh
@@ -3,6 +3,12 @@
 read -p "Please specify the image name: " imageName
 
 echo -e "Initiating image pruning for images with name '$imageName' .... \n"
-totalImages=$(crictl images | grep "$imageName" |  wc -l)
-crictl images | awk -v imageName="$imageName" '$1 ~ imageName {print $3}' | xargs -r crictl rmi
-echo -e  "Done prunning images....."
+
+crictl images | awk -v imageName="$imageName" '$1 ~ imageName {print $3}' | xargs -r crictl rmi &> /dev/null
+ec=$?
+n=0
+if [ $ec -eq 0 ]; then
+    n=$((n + 1)) 
+fi
+
+echo -e  "Done prunning $n images....."
