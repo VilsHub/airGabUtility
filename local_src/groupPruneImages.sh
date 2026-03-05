9e1c559edac3bef54f04601b29f5707d8b67c9c1
starlyvil <starlyvil@gmail.com>
2026-03-05 17:40:36 +0100

    optimised prunning logic 002

--- local_src/groupPruneImages.sh
+++ local_src/groupPruneImages.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
-
+source ../lib/progIdicator
 read -p "Please specify the image name: " imageName
 
 echo -e "Initiating image pruning for images with name '$imageName' .... \n"
+showProgress "Prunning in progress"
+success=0
+failed=0
 
-crictl images | awk -v imageName="$imageName" '$1 ~ imageName {print $3}' | xargs -r crictl rmi &> /dev/null
-ec=$?
-n=0
-if [ $ec -eq 0 ]; then
-    n=$((n + 1)) 
-fi
+while read -r img; do
+    if crictl rmi "$img" &> /dev/null; then
+        echo "Deleted image: $img"
+        success=$((success + 1))
+    else
+        echo "Failed to delete (likely in use): $img"
+        failed=$((failed + 1))
+    fi
+done < <(crictl images | awk -v imageName="$imageName" '$1 ~ imageName {print $3}')
+endProgress "Prunning  done!"  "s"
+echo
+echo "Summary:"
+echo "Successful deletions: $success"
 
-echo -e  "Done prunning $n images....."
+echo "Failed deletions: $failed"
